f471c052b11060f06a44b6ae416b60a93fda55f2
Paulo Sanchez <paulocesarsanchezespindola@gmail.com>
2021-09-18 17:34:24 -0500

    Nuevos cambios img

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
 <div class="row">
   <div class="col-sm-7">
     <br>
-        <div align="center"><img src="{{url('/img/Inicio01.png')}}"class="d-block w-300 img-fluid" alt="img" height="500"></div>
+        <div align="center"><img src="{{secure_url('/img/inicio01.png')}}"class="d-block w-300 img-fluid" alt="img" height="500"></div>
   </div>
   <div class="col-sm-4">
-  <div align="center"><img src="{{url('/img/Home.png')}}"class="d-block w-300 img-fluid" alt="img" height="500"></div>
+  <div align="center"><img src="{{secure_url('/img/Home.png')}}"class="d-block w-300 img-fluid" alt="img" height="500"></div>
   <br>
   <div class="center"><a href="https://play.google.com/store" class="btn btn-primary btn-lg active btn-lg btn-block" role="button" target="_blank" aria-pressed="true">Descarga Aquí en Playstore</a></div>
   <br>
