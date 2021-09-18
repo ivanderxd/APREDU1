97a847dc1664dbb219fc729243f0dd08a3b5cdb4
Paulo Sanchez <paulocesarsanchezespindola@gmail.com>
2021-09-17 22:04:21 -0500

    Correción de errores

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -13,7 +13,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
@@ -49,7 +49,7 @@
     <!-- Scripts -->
     
    
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
    
     @yield('scripts')
     @include('footer.footer')
